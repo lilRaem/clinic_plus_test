09316ab62a497d39b2c71793b37a4f19ae4a1ef9
Ognius Raemi <ognius.raemi@gmail.com>
2024-08-29 12:02:56 +0300

    changes from jquery to vanila js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,189 +1,140 @@
-$(document).ready(function () {
-	$('#phone').inputmask('+7 (999) 999-99-99', { placeholder: 'O' })
-	$('#phone').on('focus', function () {
-		if ($(this).val() === '') {
-			$(this).val('+7 (___) ___-__-__')
-		}
-	})
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('.boxForm').addEventListener('click', function (e) {
+		if (e.target && e.target.id === 'btn-submit') {
+			e.preventDefault()
 
-	$('#phone').on('blur', function () {
-		var input = $(this).val().replace(/\D/g, '') // Удаляем все нецифровые символы
-		if (input.length < 11) {
-			$(this).val('') // Очищаем поле ввода при потере фокуса, если введено недостаточное количество цифр
-		}
-	})
+			var currentForm = e.currentTarget.querySelector('form')
+			currentForm.querySelectorAll('.formerror').forEach(function (error) {
+				error.textContent = ''
+			})
+			ValidName(currentForm, 'name', '.formErrName')
+			ValidMail(currentForm, 'email', '.formErrEmail')
+			ValidPhone(currentForm, 'phone', '.formErrPhone')
+			if (!ValidName(currentForm, 'name', '.formErrName')) return false
+			if ($(currentForm).find('#email')[0].length != 0) {
+				if (!ValidMail(currentForm, 'email', '.formErrEmail')) return false
+			}
+			if (!ValidPhone(currentForm, 'phone', '.formErrPhone')) return false
 
-	$('.boxForm').on('click', '#btn-submit', function (e) {
-		e.preventDefault()
-
-		var currentForm = e.delegateTarget.children.form
-		$(currentForm).find('.formerror').text('')
-		if (!ValidName(currentForm, 'name', '.formErrName')) return false
-		// console.log($(currentForm).find('#email')[0])
-		if ($(currentForm).find('#email')[0].length != 0) {
-			// console.log($(currentForm).find('#email')[0].length)
-			if (!ValidMail(currentForm, 'email', '.formErrEmail')) return false
-		}
-		if (!ValidPhone(currentForm, 'phone', '.formErrPhone')) return false
-
-		$.ajax({
-			url: 'main.php',
-			type: 'POST',
-			data: {
-				name: $(currentForm).find('#name').val(),
-				email: $(currentForm).find('#email').val(),
-				phone: cleanPhoneNumber($(currentForm).find('#phone').val()),
-			},
-			success: function (response) {
-				if (response.status === 'error') {
-					console.error('Ошибка: ' + response.message)
-				} else {
-					$('.statusSend').addClass('statusSucces')
-					$('.statusSend').css({
-						opacity: '1',
-						visibility: 'visible',
-					})
-					$(currentForm).find('input').attr('disabled', 'disabled')
-					$(currentForm).find('#btn-submit').css({ cursor: 'default' })
-					$(currentForm).find('#btn-submit').hover({ 'background-color': '#2770f1' })
-					$(currentForm).find('input[type="text"], input[type="email"], input[type="tel"]').val('')
+			var xhr = new XMLHttpRequest()
+			xhr.open('POST', 'main.php', true)
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						var response = JSON.parse(xhr.responseText)
+						if (response.status === 'error') {
+							console.error('Ошибка: ' + response.message)
+						} else {
+							document.querySelector('.statusSend').classList.add('statusSucces')
+							document.querySelector('.statusSend').style.opacity = '1'
+							document.querySelector('.statusSend').style.visibility = 'visible'
+							currentForm.querySelectorAll('input').forEach(function (input) {
+								input.disabled = true
+							})
+							var submitButton = currentForm.querySelector('#btn-submit')
+							submitButton.style.cursor = 'default'
+							submitButton.style.backgroundColor = '#2770f1'
+							currentForm
+								.querySelectorAll('input[type="text"], input[type="email"], input[type="tel"]')
+								.forEach(function (input) {
+									input.value = ''
+								})
+						}
+					} else {
+						console.error('some error', xhr.statusText)
+						document.querySelector('.statusSend').innerHTML =
+							'Ошибка отправки.<br>Попробуйте обновить страницу'
+						document.querySelector('.statusSend').classList.add('statusErr')
+						document.querySelector('.statusSend').style.opacity = '1'
+						document.querySelector('.statusSend').style.visibility = 'visible'
+					}
 				}
-			},
-			error: function (xhr, status, error) {
-				console.error('some error', error, status)
-				$('.statusSend').html('Ошибка отправки.<br>Попробуйте обновить страницу')
-				$('.statusSend').addClass('statusErr')
-				$('.statusSend').css({
-					opacity: '1',
-					visibility: 'visible',
-				})
-			},
-		})
+			}
+			var params =
+				'name=' +
+				encodeURIComponent(currentForm.querySelector('#name').value) +
+				'&email=' +
+				encodeURIComponent(currentForm.querySelector('#email').value) +
+				'&phone=' +
+				encodeURIComponent(cleanPhoneNumber(currentForm.querySelector('#phone').value))
+			xhr.send(params)
+		}
 	})
 
 	function ValidName(currentForm, namefield, fielderror) {
 		var re = /^(^[А-я]{3,}$)|(^[А-я]+\s[А-я]+$)|(^[А-я]+\s[А-я]+\s[А-я]+$)$/
-		$(currentForm).find(fielderror)[0].innerHTML = ''
-		$(currentForm).find(fielderror).prev().css({
-			border: 'solid 1px #c6c6c6',
-		})
-		if (
-			$(currentForm)
-				.find('#' + namefield)
-				.val() != ''
-		) {
-			var myName = $(currentForm)
-				.find('#' + namefield)
-				.val()
-			var valid = re.test(myName.trim())
+		var errorElement = currentForm.querySelector(fielderror)
+		errorElement.innerHTML = ''
+		errorElement.previousElementSibling.style.border = 'solid 1px #c6c6c6'
+		var nameValue = currentForm.querySelector('#' + namefield).value
+		if (nameValue !== '') {
+			var valid = re.test(nameValue.trim())
 			if (!valid) {
-				output = 'Проверьте Имя!'
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-				$(currentForm).find(fielderror).prev().css({
-					border: 'solid 1px red',
-				})
+				var output = 'Проверьте Имя!'
+				errorElement.innerHTML = output
+				errorElement.previousElementSibling.style.border = 'solid 1px red'
 				return false
-			} else {
-				output = ''
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML + ' ' + output
-				return valid
 			}
+			return true
 		} else {
-			output = 'Введите свое Имя!'
-
-			$(currentForm).find(fielderror)[0].innerHTML =
-				$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-			$(currentForm).find(fielderror).prev().css({
-				border: 'solid 1px red',
-			})
+			var output = 'Введите свое Имя!'
+			errorElement.innerHTML = output
+			errorElement.previousElementSibling.style.border = 'solid 1px red'
+			return false
 		}
-		return false
 	}
 
 	function ValidPhone(currentForm, namefield, fielderror) {
 		var re =
 			/^(^7\d{10})|(^8\d{10})|(^\+\d{11})|(^\+\d+\s\(\d+\)\s\d{3}\-\d{2}-\d{2})|(^\+7\s\d{3}\s\d{3}-\d{2}-\d{2})|(^\+\d{3}\s\(\d{4}\)\s\d{5})$/
-		$(currentForm).find(fielderror)[0].innerHTML = ''
-		$(currentForm).find(fielderror).prev().css({
-			border: 'solid 1px #c6c6c6',
-		})
-		if (
-			$(currentForm)
-				.find('#' + namefield)
-				.val() != ''
-		) {
-			var myPhone = $(currentForm)
-				.find('#' + namefield)
-				.val()
-				.replace(/\D+/g, '')
-				.replace(' ', '')
-			var valid = re.test(myPhone.trim())
+		var errorElement = currentForm.querySelector(fielderror)
+		errorElement.innerHTML = ''
+		errorElement.previousElementSibling.style.border = 'solid 1px #c6c6c6'
+		var phoneValue = currentForm
+			.querySelector('#' + namefield)
+			.value.replace(/\D+/g, '')
+			.replace(' ', '')
+		if (phoneValue !== '') {
+			var valid = re.test(phoneValue.trim())
 			if (!valid) {
-				output = 'Номер телефона введен неправильно!'
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-				$(currentForm).find(fielderror).prev().css({
-					border: 'solid 1px red',
-				})
+				var output = 'Номер телефона введен неправильно!'
+				errorElement.innerHTML = output
+				errorElement.previousElementSibling.style.border = 'solid 1px red'
 				return false
-			} else {
-				output = ''
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML + ' ' + output
-				return valid
 			}
+			return true
 		} else {
-			output = 'Введите номер телефона!'
-			$(currentForm).find(fielderror)[0].innerHTML =
-				$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-			$(currentForm).find(fielderror).prev().css({
-				border: 'solid 1px red',
-			})
+			var output = 'Введите номер телефона!'
+			errorElement.innerHTML = output
+			errorElement.previousElementSibling.style.border = 'solid 1px red'
+			return false
 		}
-		return false
 	}
 
 	function ValidMail(currentForm, namefield, fielderror) {
 		var re = /^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$/
-		$(currentForm).find(fielderror)[0].innerHTML = ''
-		$(currentForm).find(fielderror).prev().css({
-			border: 'solid 1px #c6c6c6',
-		})
-		if (
-			$(currentForm)
-				.find('#' + namefield)
-				.val() != ''
-		) {
-			var myMail = $(currentForm)
-				.find('#' + namefield)
-				.val()
-			var valid = re.test(myMail)
+		var errorElement = currentForm.querySelector(fielderror)
+		errorElement.innerHTML = ''
+		errorElement.previousElementSibling.style.border = 'solid 1px #c6c6c6'
+		var mailValue = currentForm.querySelector('#' + namefield).value
+		if (mailValue !== '') {
+			var valid = re.test(mailValue)
 			if (!valid) {
-				output = 'Адрес электронной почты введен неправильно!'
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-				$(currentForm).find(fielderror).prev().css({
-					border: 'solid 1px red',
-				})
+				var output = 'Адрес электронной почты введен неправильно!'
+				errorElement.innerHTML = output
+				errorElement.previousElementSibling.style.border = 'solid 1px red'
 				return false
-			} else {
-				output = ''
-				$(currentForm).find(fielderror)[0].innerHTML =
-					$(currentForm).find(fielderror)[0].innerHTML + ' ' + output
-				return valid
 			}
+			return true
 		} else {
-			output = 'Введите адрес электронной почты!'
-			$(currentForm).find(fielderror)[0].innerHTML =
-				$(currentForm).find(fielderror)[0].innerHTML.replace(output, '') + ' ' + output
-			$(currentForm).find(fielderror).prev().css({
-				border: 'solid 1px red',
-			})
+			var output = 'Введите адрес электронной почты!'
+			errorElement.innerHTML = output
+			errorElement.previousElementSibling.style.border = 'solid 1px red'
+			return false
 		}
-		return false
 	}
+
 	function cleanPhoneNumber(phoneNumber) {
 		return phoneNumber.replace(/[+\s()-]/g, '')
 	}
